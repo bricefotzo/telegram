a0f6bb3e4989f658a24694fdcb85292ae916b8d3
Brice Fotzo <44189336+bricefotzo@users.noreply.github.com>
2024-03-06 15:26:01 +0000

    chore: Initial commit with working send message into a telegram channel with a bot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # telegram
+
+Install telegram-bot
+```python
+ pip install python-telegram-bot --upgrade
+ ```
+ Add a file .env with the variables:
+ ```shell
+TOKEN=token # the token abtained with botfather after you created yours
+CHANNEL_ID=@channel_id # the value right after t.me/
+
+ ```
+ Then run the module:
+ ```shell
+ python main.py
+ ```
---
+++ main.py
@@ -0,0 +1,42 @@
+"""
+Basic example for a bot that can send polls to a channel.
+"""
+
+import os
+import asyncio
+from telegram import Bot
+from telegram.error import NetworkError
+import logging
+
+TOKEN = os.getenv('TOKEN')
+
+# Configuration du logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def send_poll_to_channel(channel_id: str, question: str, options: list):
+    """Fonction pour envoyer un sondage.
+    
+    Args:
+        channel_id (str): ID du channel où envoyer le sondage.
+        question (str): Question du sondage.
+        options (list): Liste des options du sondage.
+        
+    Returns:
+        None
+    """
+    bot = Bot(TOKEN)
+    try:
+        # Création et envoi du sondage
+        await bot.send_poll(chat_id=channel_id, question=question, options=options, is_anonymous=False)
+        logger.info("Sondage envoyé avec succès.")
+    except (NetworkError) as e:
+        logger.error(f"Erreur lors de l'envoi du sondage : {e}")
+
+if __name__ == '__main__':
+    question = "Comment vous sentez-vous aujourd'hui ?"
+    options = ["Heureux", "Triste", "Fatigué", "Excité"]
+    CHANNEL_ID = os.getenv('CHANNEL_ID')
+    asyncio.run(send_poll_to_channel(CHANNEL_ID, question, options))
